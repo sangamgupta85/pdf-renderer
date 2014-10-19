0404d25bfda46c275d1da7765e3bbd9699a70f34
Sangam Gupta <sangamgupta85@gmail.com>
2014-10-20 00:27:31 +0530

    PDF content-type hooked into rails rendering stack

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '7c98ca1fd677a4a8a6c543d2a67c0176316caf7df4f59a0e7d31c9babcaf9b855b1b5411d34ae689c6eed89da6f74ef932ce41db3336dc2ad56c2a13ea280e79'
